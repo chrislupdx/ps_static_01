93fea3aefa8e2840aa3b9a25e5d90dd120bcf564
dreamer <wildetangeant@gmail.com>
2019-07-06 16:41:21 -0700

    made stuff blue,  added a logo to my project list, started playing with shadows

---
+++ test/renderListComponent.test.js
@@ -0,0 +1,11 @@
+import makeListComponent from '../src/listComponent';
+const test = QUnit.test;
+
+QUnit.module('makes a listcomponent');
+
+test('render test', assert => {
+    //arrange
+    
+    //act
+    //assert
+})

525c74756d84ae2ce4b585870e54c11c97e6c00b
dreamer <wildetangeant@gmail.com>
2019-07-05 17:27:13 -0700

    setup

---
+++ test/render-plain-card-test.js
@@ -0,0 +1,49 @@
+const test = QUnit.test; 
+QUnit.module('DOMcontentTest');
+
+function plainCardConstructor() {
+    const template = document.createElement('template');
+    const html = ` <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>`;
+    template.innerHTML = html;
+    const dom = template.content;
+    return dom;
+}
+
+test('Render Card Test', function(assert){
+    //arrange
+    const expected = ` <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+    //act
+    const result = plainCardConstructor();
+    
+    //assert
+    assert.htmlEqual(result, expected);
+});

eddea93c7c3cb6af85b8c64753816ce8a793f5d4
dreamer <wildetangeant@gmail.com>
2019-07-28 13:07:49 -0700

    added a preloader to work page

--- src/work.js
+++ src/work.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
+// eslint-disable-next-line no-unused-vars
     var instances = M.Dropdown.init(elems,);
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    $('.preloader-background').delay(1700).fadeOut('slow');
+
+    $('.preloader-wrapper')
+        .delay(1700)
+        .fadeOut();
 });
